426816fd674e73d419fc37982c47bc605533a1ff
Peng Jie <bivinity.pengzjie@gmail.com>
2017-12-25 15:43:52 +0800

    Add example

--- example/main.go
+++ example/main.go
@@ -1,21 +1,22 @@
 package main
 
-// import (
-// 	"log"
-//
-// 	"github.com/neighborhood999/rent/"
-// )
+import (
+	"log"
+
+	"github.com/neighborhood999/rent"
+)
 
 func main() {
-	// o := rent.NewOptions()
-	// url, err := rent.GenerateURL(*o)
-	// if err != nil {
-	// 	log.Fatalf("\x1b[91;1m%s\x1b[0m", err)
-	// }
-	//
-	// f := rent.NewFiveN1(url)
-	// f.Scrape(5)
-	//
-	// json := rent.ConvertToJSON(f.rentList)
+	o := rent.NewOptions()
+	url, err := rent.GenerateURL(*o)
+	if err != nil {
+		log.Fatalf("\x1b[91;1m%s\x1b[0m", err)
+	}
+
+	f := rent.NewFiveN1(url)
+	f.Scrape(2)
+
+	json := rent.ConvertToJSON(f.RentList)
+	log.Println(string(json))
 	// rent.ExportJSON(json)
 }
